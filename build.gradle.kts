97d2107089aafe7895017f29209d319b22c11020
Matthew Gast <incendium@gmail.com>
2022-07-28 17:28:36 -0500

    Bump Gradle + Kotlin + Diskord

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.6.21"
     application
 }
 
@@ -20,7 +20,7 @@ repositories {
 }*/
 
 dependencies {
-    implementation("com.jessecorbett:diskord-bot:2.0.0")
+    implementation("com.jessecorbett:diskord-bot:2.1.4")
 }
 
 tasks.withType<KotlinCompile> {
