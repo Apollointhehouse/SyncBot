e301f47b9f02825c8dea516e60c65f0d774229b8
Matthew Gast <incendium@gmail.com>
2022-12-27 09:53:36 -0600

    New branch for Diskord 3.x

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version "1.7.21"
     application
 }
 
@@ -20,7 +20,7 @@ repositories {
 }*/
 
 dependencies {
-    implementation("com.jessecorbett:diskord-bot:2.1.4")
+    implementation("com.jessecorbett:diskord-bot:3.0.0")
 }
 
 tasks.withType<KotlinCompile> {
