a2d1406d9cb570beb5da4f16e4a2755d70d7898f
Apollointhehouse <99405787+Apollointhehouse@users.noreply.github.com>
2024-04-22 14:31:35 +1200

    Got the bot working

--- src/main/kotlin/io/github/apollointhehouse/Main.kt
+++ src/main/kotlin/io/github/apollointhehouse/Main.kt
@@ -1,8 +1,18 @@
 package io.github.apollointhehouse
 
+import com.jessecorbett.diskord.api.channel.ChannelClient
+import com.jessecorbett.diskord.api.channel.CreateWebhook
+import com.jessecorbett.diskord.api.common.Message
+import com.jessecorbett.diskord.api.webhook.CreateWebhookMessage
+import com.jessecorbett.diskord.api.webhook.WebhookClient
+import com.jessecorbett.diskord.bot.BotContext
 import com.jessecorbett.diskord.bot.bot
 import com.jessecorbett.diskord.bot.classicCommands
-
+import com.jessecorbett.diskord.bot.events
+import com.jessecorbett.diskord.internal.client.RestClient
+import com.jessecorbett.diskord.util.isFromBot
+import com.jessecorbett.diskord.util.isFromUser
+import com.jessecorbett.diskord.util.isFromWebhook
 
 /*
  * This can be replaced with any method to load the bot token.  This specific method is provided only for convenience
@@ -19,12 +29,65 @@ private val BOT_TOKEN = try {
     )
 }
 
+private val syncedChannels = mutableSetOf<String>()
+
 suspend fun main() {
+    val client = RestClient.default(BOT_TOKEN)
+
     bot(BOT_TOKEN) {
-        classicCommands {
-            command("ping") { message ->
-                message.respond("Pong!")
-            }
+        events {
+            onMessageCreate { onMessage(it, client) }
         }
+
+        classicCommands("!sync ") {
+            command("register") { register(it, client) }
+            command("unregister") { unregister(it, client) }
+        }
+    }
+}
+
+suspend fun BotContext.register(message: Message, client: RestClient) {
+    if (message.isFromBot || message.isFromWebhook) return
+    if (!syncedChannels.add(message.channelId)) {
+        message.respond("Channel is already a synced channel")
+        return
+    }
+    ChannelClient(message.channelId, client).createWebhook(CreateWebhook("SyncBot"))
+    message.respond("Registered channel as synced channel")
+}
+
+suspend fun BotContext.unregister(message: Message, client: RestClient) {
+    if (message.isFromBot || message.isFromWebhook) return
+    if (!syncedChannels.remove(message.channelId)) {
+        message.respond("Channel is not a synced channel")
+        return
+    }
+    val webhook = ChannelClient(message.channelId, client)
+        .getWebhooks()
+        .first { it.defaultName == "SyncBot" }
+    WebhookClient(webhook.id, client).deleteWebhook()
+
+    message.respond("Unregistered channel as synced channel")
+}
+
+suspend fun onMessage(message: Message, client: RestClient) {
+    if (!message.isFromUser) return
+    if (message.content.startsWith("!sync ")) return
+    val user = message.author
+    val name = message.partialMember?.nickname ?: user.username
+    val avatarURL = "https://cdn.discordapp.com/avatars/${user.id}/${user.avatarHash}.png"
+    val content = message.content
+
+    val channelIDs = syncedChannels
+        .asSequence()
+        .takeIf { message.channelId in it }
+        ?.filter { it != message.channelId } ?: return
+
+    for (channelID in channelIDs) {
+        val webhook = ChannelClient(channelID, client)
+            .getWebhooks()
+            .first { it.defaultName == "SyncBot" }
+
+        WebhookClient(webhook.id, client).execute(webhook.token ?: "", CreateWebhookMessage(content, name, avatarURL))
     }
 }
